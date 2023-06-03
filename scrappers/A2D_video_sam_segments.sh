5bde87da2bcae81ac7c6f1a802e3e2af80bfcf57
unknown <daishlu@gmail.com>
2023-06-02 23:28:49 -0700

    use SAM huge model

--- scrappers/A2D_video_sam_segments.sh
+++ scrappers/A2D_video_sam_segments.sh
@@ -7,6 +7,6 @@ SAM_MODEL_DIR="/home/shuangludai/ai_models/segment_anything"
 mkdir -p $A2D_CLIPS_OUTPUT_DIR
 python3 tools/sam_video_segments.py\
   --sam_model_dir=$SAM_MODEL_DIR\
-  --sam_model_type="vit_b"\
+  --sam_model_type="vit_h"\
   --clips_path=$A2D_CLIPS_PATH\
   --output_clips_dir=$A2D_CLIPS_OUTPUT_DIR
