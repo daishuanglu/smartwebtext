b1863e1f56f39d6b402093914ab47ae4866f3183
unknown <daishlu@gmail.com>
2024-05-11 20:08:16 -0700

    init kaggle

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+LOGGER_DIR = 'trainer_log'
+EVAL_DIR = 'evaluations'
+
+SCORE_FIELD = '{model}:{field}:score'    
+PRED_FIELD = '{model}:{field}:value'
+TEST_CONCEPTS = ['CasualCode', 'PartCostsBucket', 'LaborHoursBucket']
