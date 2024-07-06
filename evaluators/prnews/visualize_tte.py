22dfc8e1d47c144be2dcc5ed8f22f627f414bae0
unknown <daishlu@gmail.com>
2024-07-06 13:51:45 -0700

    update tte embedding vz

--- evaluators/prnews/visualize_tte.py
+++ evaluators/prnews/visualize_tte.py
@@ -4,10 +4,22 @@ import json
 from models import cf
 from utils import train_utils, visual_utils
 from preprocessors import pipelines
-from evaluators.prnews import assets
+import pandas as pd
 
 
 OUTPUT_DIR = 'evaluation/prnews/tte_embedding_projection'
+
+
+def load_eval_samples(eval_data_path: str, query_col: str):
+    df = pd.read_csv(
+        eval_data_path, 
+        dtype=str, 
+        sep=pipelines.PRNEWS_DATA_SEP, 
+        parse_dates=False, 
+        keep_default_na=False, 
+        na_values=[])
+    df = df.sample(frac=0.01)
+    return df[query_col].to_list()
 
 
 def main():
@@ -17,25 +29,26 @@ def main():
             output_files=[config['train_data_path'], config['val_data_path']],
             split_ratio=config['train_val_split_ratio'],
             vocabs={config['item_vocab_path']: [config['ref_col']]})
-    with open(config['item_vocab_path'], 'r') as f:
-        ref_vocab = json.load(f)
-    ref_vocab = list(ref_vocab.keys())
+    
     logger_dir = config.get('logger_dir', train_utils.DEFAULT_LOGGER_DIR)
     model_obj = cf.TTEModel(config).to(train_utils.device)
     latest_ckpt_path = train_utils.latest_ckpt(logger_dir, config['model_name'])
     model_obj = train_utils.load(model_obj, latest_ckpt_path)
     model_obj.eval()
     # Company embeddings
+    with open(config['item_vocab_path'], 'r') as f:
+        ref_vocab = json.load(f)
     all_companies = ['' for _ in ref_vocab]
     for name, idx in ref_vocab.items():
         all_companies[idx] = name
     company_embeddings = model_obj.item_embedding.data.weights
-    visual_utils.tensorboard_text_embedding_v2(
-        os.path.join(OUTPUT_DIR, 'companies'), all_companies, company_embeddings)
-    news_embeddings = model_obj.query_model({
-        config['ref_col']: assets.TEST_NEWS_QUERIES})
     visual_utils.tensorboard_text_embedding(
-        os.path.join(OUTPUT_DIR, 'news'), assets.TEST_NEWS_QUERIES, news_embeddings)
+        os.path.join(OUTPUT_DIR, 'companies'), all_companies, company_embeddings)
+    # News embeddings
+    eval_news = load_eval_samples(config['val_data_path'], 'Title')
+    news_embeddings = model_obj.query_model({config['ref_col']: eval_news})
+    visual_utils.tensorboard_text_embedding(
+        os.path.join(OUTPUT_DIR, 'news'), eval_news, news_embeddings)
 
 
 if __name__ == '__main__':
